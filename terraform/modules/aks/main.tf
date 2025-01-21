60d7888a0933e2aedfe2c14a4bb1f5dd62a54bd3
Blends <110492870+StickableIL@users.noreply.github.com>
2025-01-21 12:26:35 +0200

    Refactor Terraform configuration to streamline AKS module and add upgrade settings

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -13,6 +13,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
     tags = {
       Environment = var.environment
     }
+
+    upgrade_settings {
+      max_surge = "10%"
+    }
   }
 
   identity {
@@ -45,5 +49,4 @@ resource "azurerm_role_assignment" "aks_acr" {
   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                           = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
 }
